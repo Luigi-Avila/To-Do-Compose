854728e9af2639e7ae0eacc603e9a668ae211f8b
Luigi-Avila <l.avila.uvg@gmail.com>
2023-01-30 18:01:18 -0600

    feat: Fab button added V.0.1.0

--- app/src/main/java/com/example/to_do_compose/addTasks/ui/TasksScreen.kt
+++ app/src/main/java/com/example/to_do_compose/addTasks/ui/TasksScreen.kt
@@ -1,8 +1,31 @@
 package com.example.to_do_compose.addTasks.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun TasksScreen() {
+    Box(modifier = Modifier.fillMaxSize()){
+        FabDialog(Modifier.align(Alignment.BottomEnd))
+    }
+}
 
 @Composable
-fun TasksScreen(tasksViewModel: TasksViewModel) {
-
+fun FabDialog(modifier: Modifier) {
+    FloatingActionButton(onClick = {  }, modifier = modifier
+        .padding(16.dp)) {
+        Icon(imageVector = Icons.Filled.Add, contentDescription ="Icon add")
+    }
 }
