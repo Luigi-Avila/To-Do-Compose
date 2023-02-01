c6d4642060245850183c29ff883365aa468a0fdc
luigi <l.avila.uvg@gmail.com>
2023-01-31 20:49:04 -0600

    feat: logic about create task added V.0.4.0

--- app/src/main/java/com/example/to_do_compose/addTasks/ui/TasksScreen.kt
+++ app/src/main/java/com/example/to_do_compose/addTasks/ui/TasksScreen.kt
@@ -2,6 +2,8 @@ package com.example.to_do_compose.addTasks.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -18,6 +20,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.to_do_compose.addTasks.ui.model.TaskModel
 
 
 @Composable
@@ -31,6 +34,37 @@ fun TasksScreen(tasksViewModel: TasksViewModel) {
             onDismiss = { tasksViewModel.onDialogClose() },
             onTaskAdded = { tasksViewModel.onTaskCreated(it) })
         FabDialog(Modifier.align(Alignment.BottomEnd), tasksViewModel)
+        TasksList(tasksViewModel)
+    }
+}
+
+@Composable
+fun TasksList(tasksViewModel: TasksViewModel) {
+    val myTasks: List<TaskModel> = tasksViewModel.tasks
+    LazyColumn() {
+        items(myTasks, key = { it.id }) { task ->
+            ItemTask(taskModel = task, tasksViewModel = tasksViewModel)
+        }
+    }
+}
+
+@Composable
+fun ItemTask(taskModel: TaskModel, tasksViewModel: TasksViewModel) {
+    Card(
+        Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp), elevation = 8.dp
+    ) {
+        Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = taskModel.task, modifier = Modifier
+                    .padding(horizontal = 4.dp)
+                    .weight(1f)
+            )
+            Checkbox(
+                checked = taskModel.selected,
+                onCheckedChange = { tasksViewModel.onCheckBoxSelected(taskModel) })
+        }
     }
 }
 
@@ -73,6 +107,7 @@ fun AddTaskDialog(show: Boolean, onDismiss: () -> Unit, onTaskAdded: (String) ->
                 Spacer(modifier = Modifier.size(16.dp))
                 Button(onClick = {
                     onTaskAdded(myTask)
+                    myTask = ""
                 }, modifier = Modifier.fillMaxWidth()) {
                     Text(text = "Añadir tarea")
                 }
